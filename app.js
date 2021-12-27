9bd8399b145701e8e0c25bca71096f90bca0384d
black <kamil.ganczarski9@gmail.com>
2021-12-27 12:07:23 +0100

    Add database connection

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+require('dotenv').config()
+require('express-async-errors')
+
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require("express")
 const app = express()
 
-const PORT = process.env.PORT || 3001
+const connectDB = require('./server/db/connect')
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`)
-})
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+const port = process.env.PORT || 3001
+
+const start = async () => {
+  try {
+    // connectDB
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => console.log(`Server is listening on port ${port}...`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
